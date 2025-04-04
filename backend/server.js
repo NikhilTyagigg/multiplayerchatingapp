0f7eaac05a60013d34b32775eb59d3b9fe5799d6
Nikhil Tyagi <105928333+NikhilTyagigg@users.noreply.github.com>
2025-04-04 16:12:59 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const io = socketIo(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
+PORT = process.env.PORT || 5000;
 
 let users = {}; // Stores connected users and positions
 
@@ -73,4 +74,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => console.log("Server running on port 5000"));
+server.listen(PORT, () => console.log("Server running on port `PORT`"));
